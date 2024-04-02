0c65face3294861ca78670a2998c2d3d98c04b4f
Jason Hankins <tul53850@temple.edu>
2024-04-02 12:20:41 -0400

    Intent

--- app/src/main/java/edu/temple/xkcdreader/MainActivity.kt
+++ app/src/main/java/edu/temple/xkcdreader/MainActivity.kt
@@ -1,7 +1,10 @@
 package edu.temple.xkcdreader
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -39,9 +42,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        intent.action?.run{
+            if(this == Intent.ACTION_VIEW) {
+                intent.data?.let {
+                    lifecycleScope.launch(Dispatchers.Main){
+                        fetchComic(it.path!!)
+                    }
+                }
+            }
+
+            findViewById<Button>(R.id.requestButton).setOnClickListener{
+                intent = Intent(ACTION_APP_OPEN_BY_DEFAULT_SETTINGS,
+                    Uri.parse("package:${packageName}"))
+                startActivity(intent)
+            }
+        }
+
     }
 
-    suspend fun fetchComic(comicId: String) {
+    private suspend fun fetchComic(comicId: String) {
 
         val jsonObject: JSONObject
 
